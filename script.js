bda27429451abb7a9e2308c5441e99a8f2593693
TCHUOI <tcui@bostonk12.org>
2024-09-16 08:27:17 -0400

    Create script.js

---
+++ script.js
@@ -0,0 +1,8 @@
+let canvas = document.getElementById("canvi");
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+window.innerHeight = 400;
+window.innerWidth = 400;
+
+let ctx = canvas.getContext("2d");
