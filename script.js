7efe1b6760bad84d32c189ae4c5abfea8bd86d51
TCHUOI <tcui@bostonk12.org>
2024-09-16 08:28:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -6,3 +6,9 @@ window.innerHeight = 400;
 window.innerWidth = 400;
 
 let ctx = canvas.getContext("2d");
+
+
+setInterval(function() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.fillRect(Math.random() * 100, 0, 50, 50);
+}, 17);
